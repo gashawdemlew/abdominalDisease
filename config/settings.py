6e98786941a5a63173d587eeae9d3cbf9af162d5
Gashaw D <42271526+gashawdemlew@users.noreply.github.com>
2024-06-17 20:44:44 +0300

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,21 @@
+import os, sys
+import importlib.util
+import shutil
+import logging
+from dataclasses import dataclass
+import os
+import subprocess
+
+cdir = os.path.dirname(os.path.realpath(__file__))
+path = cdir
+
+cur_dir = os.getcwd()
+parent_dir = os.path.realpath(os.path.join(os.path.dirname(cur_dir)))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+    sys.path.append(cur_dir)
+sys.path.insert(1, ".")
+
+@dataclass
+class ConfigStaticFiles:
+  haggingFace_url = "gashudemman/abdomenDiseaseDetector"
